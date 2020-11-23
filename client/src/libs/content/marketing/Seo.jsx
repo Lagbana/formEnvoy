601be974f4c2959d0ffb41dd8cb81d24442c2b75
Larry Agbana <larryagbana@gmail.com>
2020-11-22 19:05:44 -0500

    updates

---
+++ client/src/libs/content/marketing/Seo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SectionOneMessaging = () => {
+  return <div>Test</div>;
+};
+const SectionTwoMessaging = () => {
+  return <div>Test</div>;
+};
+const SectionThreeMessaging = () => {
+  return <div>Test</div>;
+};
+const FooterSectionMessaging = () => {     
+  return <div>Test</div>;
+};
+
+export {
+  SectionOneMessaging,
+  SectionTwoMessaging,
+  SectionThreeMessaging,
+  FooterSectionMessaging,
+};
